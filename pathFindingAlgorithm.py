02e2d76eef75d7039e85ccfc4eda865a9f7fda0b
jkallson <janar.kallson@gmail.com>
2020-05-21 14:49:41 +0300

    Changed how the initial grid is created, added possibility to change color of grid elements.

--- pathFindingAlgorithm.py
+++ pathFindingAlgorithm.py
@@ -1,29 +1,54 @@
 import pygame
 
 positions = []
+
+#Method which is used to build a grid
 def buildGrid(y,w):
-    for i in range(1,21):
-        x = 20
-        y = y+20
-        for j in range(1,31):
-            pygame.draw.line(screen, [0, 0, 0], [x,y], [x+w,y])
-            pygame.draw.line(screen, [0, 0, 0], [x + w, y], [x + w, y+w])
-            pygame.draw.line(screen, [0, 0, 0], [x + w, y+w], [x, y+w])
-            pygame.draw.line(screen, [0, 0, 0], [x, w+y], [x , y])
-            positions.append((x,y))
-            x = x+20
+    #Creating a grid
+    for z in range(0, 500, w):
+        row = []
+        for x in range(0, y, w):
+            #Creating a rectangle
+            rect = pygame.Rect(x, z, w - 1, w - 1)
+            #Adding this retangle to row
+            row.append([rect, (0, 0, 0)])
+        #Adding created row to the positions list
+        positions.append(row)
 
+#Method which is used to update the screen
+def screenUpdate():
+    for row in positions:
+        for item in row:
+            rect, color = item
+            pygame.draw.rect(screen, color, rect)
+    pygame.display.flip()
 
-    pygame.display.update()
-#Loome mängu
+#Creating the game
 pygame.init()
 
-screen = pygame.display.set_mode((650,450))
+#Creating a screen
+screen = pygame.display.set_mode((800,500))
 screen.fill([255, 255, 255])
 pygame.display.update()
-buildGrid(0,20)
+#Building the grid
+buildGrid(800,20)
 running = True
 while running:
+    #When program is closed
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             running = False
+
+        #When user clicks somewhere in the grid
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            #Check which rect was clicked and change its color on list
+            for row in positions:
+                for item in row:
+                    rect, color = item
+                    if rect.collidepoint(event.pos):
+                        if color == (0, 255, 0):
+                            item[1] = (255, 0, 0)
+                        else:
+                            item[1] = (0, 255, 0)
+    #Updates the screen
+    screenUpdate()
