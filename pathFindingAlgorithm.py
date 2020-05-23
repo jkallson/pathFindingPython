fcf2f9a219d3470b85d2b8b2281cea28140d68ed
jkallson <janar.kallson@gmail.com>
2020-05-23 10:41:51 +0300

    Dijkstra algorithm now works correctly. Added functionality to show the shortest path.

--- pathFindingAlgorithm.py
+++ pathFindingAlgorithm.py
@@ -5,7 +5,6 @@ import pygame
 positions = []
 #Method which is used to build a grid
 def buildGrid(y,w):
-
     #Distance used for infinity
     maxInt = 99999
     #Creating a grid
@@ -15,7 +14,7 @@ def buildGrid(y,w):
             #Creating a rectangle
             rect = pygame.Rect(x, z, w - 1, w - 1)
             #Adding this retangle to row
-            row.append([rect, (0, 0, 0),maxInt,False])
+            row.append([rect, (0, 0, 0),maxInt,False,[999,999]])
         #Adding created row to the positions list
         positions.append(row)
 
@@ -23,64 +22,95 @@ def buildGrid(y,w):
 def screenUpdate():
     for row in positions:
         for item in row:
-            rect, color, distance, visited = item
+            rect, color, distance, visited,previousNode = item
             pygame.draw.rect(screen, color, rect)
     pygame.display.flip()
 
 #Function which is used to decide if distance needs to be changed
-def changeDistance(row,rowElement,distance):
+def changeDistance(row,rowElement,distance,previousRow,previousElement):
     #If distance through some node is smaller that the existing distance, then change it
     if(distance+1 < positions[row][rowElement][2]):
         positions[row][rowElement][2] = distance + 1
-
+        positions[row][rowElement][4][0] = previousRow
+        positions[row][rowElement][4][1] = previousElement
 #Find neighbours
 def checkNeighBours(row,rowElement, distance):
     #4 neighbours
     if(row-1>=0 and rowElement-1>= 0 and row != 24 and rowElement != 39):
-        changeDistance(row,rowElement+1,distance)
-        changeDistance(row+1, rowElement, distance)
-        changeDistance(row-1, rowElement, distance)
-        changeDistance(row, rowElement - 1, distance)
+        changeDistance(row,rowElement+1,distance,row,rowElement)
+        changeDistance(row+1, rowElement, distance,row,rowElement)
+        changeDistance(row-1, rowElement, distance,row,rowElement)
+        changeDistance(row, rowElement - 1, distance,row,rowElement)
     #Left upper corner
     elif (row -1 == -1 and rowElement-1 == -1):
-        changeDistance(row, rowElement + 1, distance)
-        changeDistance(row+1, rowElement, distance)
+        changeDistance(row, rowElement + 1, distance,row,rowElement)
+        changeDistance(row+1, rowElement, distance,row,rowElement)
     #Top row
     elif (row -1 == -1 and rowElement-1 >= 0 and rowElement != 39):
-        changeDistance(row, rowElement + 1, distance)
-        changeDistance(row+1, rowElement, distance)
-        changeDistance(row, rowElement - 1, distance)
+        changeDistance(row, rowElement + 1, distance,row,rowElement)
+        changeDistance(row+1, rowElement, distance,row,rowElement)
+        changeDistance(row, rowElement - 1, distance,row,rowElement)
 
     # Left bottom corner
     elif (row == 24 and rowElement - 1 == -1):
-        changeDistance(row, rowElement + 1, distance)
-        changeDistance(row - 1, rowElement, distance)
+        changeDistance(row, rowElement + 1, distance,row,rowElement)
+        changeDistance(row - 1, rowElement, distance,row,rowElement)
 
     #Left column
     elif (row -1 >= 0 and rowElement-1 == -1):
-        changeDistance(row, rowElement + 1, distance)
-        changeDistance(row+1, rowElement, distance)
-        changeDistance(row-1, rowElement, distance)
+        changeDistance(row, rowElement + 1, distance,row,rowElement)
+        changeDistance(row+1, rowElement, distance,row,rowElement)
+        changeDistance(row-1, rowElement, distance,row,rowElement)
     #Top right
     elif (row == 0 and rowElement  == 39):
-        changeDistance(row, rowElement-1, distance)
-        changeDistance(row +1, rowElement, distance)
+        changeDistance(row, rowElement-1, distance,row,rowElement)
+        changeDistance(row +1, rowElement, distance,row,rowElement)
     #Bottom right
     elif (row == 24 and rowElement == 39):
-        changeDistance(row, rowElement - 1, distance)
-        changeDistance(row - 1, rowElement, distance)
+        changeDistance(row, rowElement - 1, distance,row,rowElement)
+        changeDistance(row - 1, rowElement, distance,row,rowElement)
 
     #Right column
     elif (row > 0 and rowElement == 39):
-        changeDistance(row, rowElement - 1, distance)
-        changeDistance(row + 1, rowElement, distance)
-        changeDistance(row - 1, rowElement, distance)
+        changeDistance(row, rowElement - 1, distance,row,rowElement)
+        changeDistance(row + 1, rowElement, distance,row,rowElement)
+        changeDistance(row - 1, rowElement, distance,row,rowElement)
 
     #Bottom row
     elif (row == 24 and rowElement > 0):
-        changeDistance(row, rowElement + 1, distance)
-        changeDistance(row, rowElement-1, distance)
-        changeDistance(row - 1, rowElement, distance)
+        changeDistance(row, rowElement + 1, distance,row,rowElement)
+        changeDistance(row, rowElement-1, distance,row,rowElement)
+        changeDistance(row - 1, rowElement, distance,row,rowElement)
+
+#Function which returns the shortest path elements
+def findShortestPathElements(endRow,endElement):
+    shortestPathElements = []
+    #Getting the ending node
+    node = positions[endRow][endElement]
+    #While there is still previous elements, lets continue the loop
+    while node[4] != [999,999]:
+        #Finding previous node position
+        element = [node[4][0],node[4][1]]
+        #Appending it to the list
+        shortestPathElements.append(element)
+        #Selecting the previous node as our new node
+        node = positions[node[4][0]][node[4][1]]
+    #Reversing the list so we will begin from the start node
+    shortestPathElements.reverse()
+    return shortestPathElements
+
+#Function which is used to visualize the shortest path
+def visualizeShortestPath(shortestPath):
+    #This loop will go through all of the elements that are in the shortest path and change their color
+    for item in shortestPath:
+        #Selecting a node
+        row = item[0]
+        rowElement = item[1]
+        #Changing its color to yellow
+        positions[row][rowElement][1] = [241,255,31]
+        #Updating the screen
+        screenUpdate()
+
 #Function which is used to find the lowest node
 def findLowestNode():
     lowestX = 0
@@ -90,7 +120,7 @@ def findLowestNode():
     for i,row in enumerate(positions):
         #Iterating through all row nodes
         for j,item in enumerate(row):
-            rect, color, distance, visited = item
+            rect, color, distance, visited,previousNode = item
             #If we have not visited the node and it has smaller best distance then lets keep that in memory
             if distance < currentBestDistance and not visited:
                 #Row number
@@ -99,7 +129,7 @@ def findLowestNode():
                 lowestY = j
                 currentBestDistance = distance
     return lowestX,lowestY
-#todo! Add a way to show the shortest path (should use backtracking)
+
 #Dijkstra algorithm
 def dijkstra(startx, starty, endx, endy):
     #Define the starting point
@@ -124,7 +154,6 @@ def dijkstra(startx, starty, endx, endy):
         distance = node[2]
         # Finding all of its neighbours and updating their distance
         checkNeighBours(nodeX,nodeY, distance)
-
         #Mark that node is visited
         node[3] = True
         #Change node color
@@ -132,8 +161,11 @@ def dijkstra(startx, starty, endx, endy):
         screenUpdate()
 
         time.sleep(0.002)
+    #Changing end node color
     endNode[1] = (255, 0, 0)
-
+    #Getting the shortest path elements and then visualizing it
+    shortestPath = findShortestPathElements(endx,endy)
+    visualizeShortestPath(shortestPath)
 #Creating the game
 pygame.init()
 
@@ -144,7 +176,6 @@ pygame.display.update()
 #Building the grid
 buildGrid(800,20)
 running = True
-
 
 while running:
     #When program is closed
@@ -157,7 +188,7 @@ while running:
             #Check which rect was clicked and change its color on list
             for row in positions:
                 for item in row:
-                    rect, color,distance,visited = item
+                    rect, color, distance, visited,previousNode = item
                     if rect.collidepoint(event.pos):
                         if color == (0, 255, 0):
                             item[1] = (255, 0, 0)
@@ -168,7 +199,7 @@ while running:
         elif event.type == pygame.KEYDOWN:
             # To activate Dijkstra algorithm
             if(event.key == pygame.K_SPACE):
-                dijkstra(0,0,10,4)
+                dijkstra(15,20,0,0)
             #To reset the screen user has to press "r"
             elif(event.key == pygame.K_r):
                 positions = []
